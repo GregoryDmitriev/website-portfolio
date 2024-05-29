fd78aa3bb0139269f8cb2d4c4ed10b2fd1d77253
GregoryDmitriev <bjoldgv@gmail.com>
2024-05-29 15:48:08 +0300

    Delete AOS

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
 		<section id='home' className={styles.home}>
 			<div className={styles.container}>
 				<div className={styles.content}>
-					<h1 data-aos='fade-up' data-aos-duration='800' data-aos-delay='300' className={`${styles.title}`}>Hi, I'm Gregory Dmitriev</h1>
+					<h1 className={`${styles.title}`}>Hi, I'm Gregory Dmitriev</h1>
 					<p>
 						I am a frontend web developer. I can provide clean code and pixel
 						perfect design. I also make website more & more interactive with web
